6081fe85c1078f4e882a4af884b033bebaca5368
Strikelab <contact@strikelab.fr>
2023-11-30 18:22:46 +0100

    whatgpt3

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./features.scss";
-function Features() {
-  return <div>WhatGPT3</div>;
+function Features(props) {
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient-text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default Features;
