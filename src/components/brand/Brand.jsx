f2c5d818a09386f9e91b66a9e739ee90910982de
Strikelab <contact@strikelab.fr>
2023-11-30 15:40:43 +0100

    brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import "./brand.scss";
+import { google, atlassian, slack, shopify, dropbox } from "./imports";
 function Brand() {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section-padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 }
 
 export default Brand;
